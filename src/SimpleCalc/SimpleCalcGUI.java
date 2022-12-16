c5c2f6fa43b575ec0d10f9a97294f49c6ef239c0
Gantuangco <gantuangco98@yahoo.com.ph>
2022-12-17 06:14:07 +0800

    Complete SimpleCalc

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -1,6 +1,8 @@
 package SimpleCalc;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class SimpleCalcGUI extends JFrame {
     private JPanel pnlMain;
@@ -17,5 +19,36 @@ public class SimpleCalcGUI extends JFrame {
         app.setSize(768, 400);
         app.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         app.setVisible(true);
+    }
+
+    public SimpleCalcGUI() {
+        cbOperations.addItem("+");
+        cbOperations.addItem("-");
+        cbOperations.addItem("*");
+        cbOperations.addItem("/");
+
+        btnCompute.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int num1 = Integer.parseInt(tfNumber1.getText()), num2 = Integer.parseInt(tfNumber2.getText());
+                int result = 0;
+                String op = (String)cbOperations.getSelectedItem();
+                switch (op) {
+                    case "+" :
+                        result = num1 + num2;
+                        break;
+                    case "-" :
+                        result = num1 - num2;
+                        break;
+                    case "*" :
+                        result = num1 * num2;
+                        break;
+                    case "/" :
+                        result = num1 / num2;
+                        break;
+                }
+                lblResult.setText(String.valueOf(result));
+            }
+        });
     }
 }
