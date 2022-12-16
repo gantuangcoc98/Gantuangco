4d7d3154877d7736cab14361098a102ab23cfdb9
Gantuangco <gantuangco98@yahoo.com.ph>
2022-12-17 04:41:05 +0800

    Add LeapYear main function

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -1,12 +1,23 @@
 package LeapYear;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.InputMismatchException;
 
-public class LeapYearGUI extends JFrame {
+public class LeapYearGUI extends JFrame{
     private JPanel pnlMain;
     private JTextField tfYear;
     private JButton btnCheckYear;
+
+    public static void main(String[] args) {
+        LeapYearGUI app = new LeapYearGUI();
+        app.setContentPane(app.pnlMain);
+        app.setSize(500, 250);
+        app.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        app.setVisible(true);
+    }
+
 }
 
