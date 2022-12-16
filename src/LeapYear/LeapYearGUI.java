7287738382ec404caeeadcd90f424e260c7cd542
Gantuangco <gantuangco98@yahoo.com.ph>
2022-12-17 05:10:30 +0800

    Add exception handling to LeapYear

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -18,22 +18,34 @@ public class LeapYearGUI extends JFrame{
                 int input = Integer.parseInt(tfYear.getText());
                 String message = "";
 
-                boolean leap_year = false;
-                if (input % 4 == 0) {
-                    if (input % 100 == 0) {
-                        if (input % 400 == 0) {
-                            leap_year = true;
-                        } else {
-                            leap_year = false;
+                try {
+                    if (input == 0)
+                        throw new zeroError();
+                    else if (input < 0)
+                        throw new less_zeroError();
+                    else {
+                        boolean leap_year = false;
+                        if (input % 4 == 0) {
+                            if (input % 100 == 0) {
+                                if (input % 400 == 0) {
+                                    leap_year = true;
+                                } else {
+                                    leap_year = false;
+                                }
+                            } else {
+                                leap_year = true;
+                            }
                         }
-                    } else {
-                        leap_year = true;
+                        if (leap_year)
+                            message = "Leap year";
+                        else
+                            message = "Not a leap year";
                     }
+                } catch (zeroError ze) {
+                    message = "Input must be a non-zero integer";
+                } catch (less_zeroError lze) {
+                    message = "Input must not be less than zero";
                 }
-                if (leap_year)
-                    message = "Leap year";
-                else
-                    message = "Not a leap year";
                 JOptionPane.showMessageDialog(btnCheckYear, message);
             }
         });
@@ -45,6 +57,14 @@ public class LeapYearGUI extends JFrame{
         app.setSize(500, 250);
         app.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         app.setVisible(true);
+    }
+
+    public class zeroError extends Throwable {
+        public zeroError() {}
+    }
+
+    public class less_zeroError extends Throwable {
+        public less_zeroError() {}
     }
 }
 
