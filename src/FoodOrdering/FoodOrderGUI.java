44b70e1a0991aebc2c60d3087697b24d9368809a
Gantuangco <gantuangco98@yahoo.com.ph>
2022-12-17 06:28:27 +0800

    Add FoodOrder main function

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -1,8 +1,10 @@
 package FoodOrdering;
 
+import SimpleCalc.SimpleCalcGUI;
+
 import javax.swing.*;
 
-public class FoodOrderGUI {
+public class FoodOrderGUI extends JFrame {
     private JPanel pnlMain;
     private JCheckBox cPizza;
     private JRadioButton rbNone;
@@ -15,4 +17,13 @@ public class FoodOrderGUI {
     private JRadioButton rb5;
     private JRadioButton rb10;
     private JRadioButton rb15;
+
+    public static void main(String[] args) {
+        FoodOrderGUI app = new FoodOrderGUI();
+        app.setContentPane(app.pnlMain);
+        app.setTitle("Simple Calculator");
+        app.setSize(768, 768);
+        app.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        app.setVisible(true);
+    }
 }
